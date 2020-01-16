f0088a89561e94afcd6e029b31c5c0af1972b66a
Octavio Palomeque <octaviop_95@hotmail.com>
2020-01-15 19:27:48 -0600

    Fixed script to start

--- app.js
+++ app.js
@@ -77,6 +77,6 @@ app.use("/", authRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000, function () {
-    console.log("Listening in port 3000...")
+app.listen((process.env.PORT || 3000), function () {
+    console.log("Listening in port " + process.env.PORT)
 })
